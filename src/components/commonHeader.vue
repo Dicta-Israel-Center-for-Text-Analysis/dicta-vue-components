782fa02bcf0b13013f5abcaf307e7c0c41f28b28
Aryeh Sanders <aryehsanders@gmail.com>
2019-03-20 16:08:24 +0200

    update header

--- src/components/commonHeader.vue
+++ src/components/commonHeader.vue
@@ -1,111 +1,41 @@
 <template>
-  <div class="container">
-  <header class="navbar navbar-default top-header d-none d-lg-flex">
-        <div class="navbar-nav mr-auto">
-          <a href="http://dicta.org.il/index.html">
-            <em class="f black title">DICTA</em>
-            <span class="txt add f black">Analytical tools for Jewish texts</span>
-          </a>
-        </div>
-        <ul class="nav navbar-nav nav-mega">
-          <li><a class="eng" @click="changeLanguage" href="#">עברית</a></li>
-          <li class="dropdown header-dropdown">
-            <a @click="openDropDown" href="#" class="link mega-dropdown f black" id="dropdownMenu2">
-              DICTA Tools<img class="link-icon" src="../assets/Triangle-down.png">
+  <div :dir="hebrew? 'rtl' : 'ltr'">
+    <div class="top-bar">
+      <span class="top-bar-left">
+        <a href="http://dicta.org.il/index.html">
+          <span class="dicta">DICTA</span>
+          &nbsp;
+          <span class="dicta-tagline">Analytical tools for Hebrew texts</span>
+        </a>
+      </span>
+      <span class="top-bar-right">
+        <a @click="changeLanguage" href="#">עברית</a>
+        |
+        <a @click="toggleDropDown">
+          DICTA Tools <img src="../assets/Triangle-down.png">
+        </a>
+      </span>
+    </div>
+    <div v-if="menuOpen" class="popup">
+      <div class="popup-back" @click="toggleDropDown"></div>
+      <div class="tool-bar">
+        <ul class="tool-list">
+          <li class="tool" v-for="tool in tools" :key="tool.logo">
+            <a :href="tool.href" class="tool-link" target="_blank">
+              <img class="logo" alt="logo" :src="tool.logo">
+              <div class="description">
+                <div class="title">{{hebrew ? tool.hebTitle : tool.engTitle}}</div>
+                <div class="subtitle">{{hebrew ? tool.hebSubtitle : tool.engSubtitle}}</div>
+              </div>
             </a>
-            <ul class="dropdown-menu mega-menu"
-                :class="{show: menuOpen}">
-                <div class="container">
-                  <div class="row mega-menu-row">
-                    <div class="mega-menu-col col-lg-4 col-md-4 col-sm-6 col-xs-12">
-                      <a href="http://nakdan.dicta.org.il/" target="_blank" class="media">
-                        <div class="media-left">
-                          <img src="../assets/Logo_Nakdan_Black_Circle.png" class="img media-object">
-                        </div>
-                        <div class="media-body">
-                          <h4 class="media-heading">Nakdan</h4>
-                          <p class="sub-heading">Add nikud to Modern / Rabbinic / Poetry text</p>
-                        </div>
-                      </a>
-                    </div>
-                    <div class="mega-menu-col col-lg-4 col-md-4 col-sm-6 col-xs-12">
-                      <a href="http://classify.dicta.org.il" target="_blank" class="media">
-                        <div class="media-left">
-                          <img src="../assets/Logo_Classify_Black_Circle.png" class="img media-object">
-                        </div>
-                        <div class="media-body">
-                          <h4 class="media-heading">Bible Classification</h4>
-                          <p class="sub-heading">Categorization, based on key styles markers</p>
-                        </div>
-                      </a>
-                    </div>
-                    <div class="mega-menu-col col-lg-4 col-md-4 col-sm-6 col-xs-12">
-                      <a href="http://classify.dicta.org.il/#/" target="_blank" class="media">
-                        <div class="media-left">
-                          <img src="../assets/Logo_Synoptic_Black_Circle.png" class="img media-object">
-                        </div>
-                        <div class="media-body">
-                          <h4 class="media-heading">Synopsis Builder</h4>
-                          <p class="sub-heading">Aligns versions of the same text</p>
-                        </div>
-                      </a>
-                    </div>
-                    <div class="mega-menu-col col-lg-4 col-md-4 col-sm-6 col-xs-12">
-                      <a href="http://search.dicta.org.il/" target="_blank" class="media">
-                        <div class="media-left">
-                          <img src="../assets/Logo_Search_Black_Circle.png" class="img media-object">
-                        </div>
-                        <div class="media-body">
-                          <h4 class="media-heading">Search the Bible</h4>
-                          <p class="sub-heading">Easily search in the Bible</p>
-                        </div>
-                      </a>
-                    </div>
-                    <div class="mega-menu-col col-lg-4 col-md-4 col-sm-6 col-xs-12">
-                      <a href="http://pasuk.dicta.org.il/" target="_blank" class="media">
-                        <div class="media-left">
-                          <img src="../assets/Logo_PasukFinder_Black_Circle.png" class="img media-object">
-                        </div>
-                        <div class="media-body">
-                          <h4 class="media-heading">Pasuk Finder</h4>
-                          <p class="sub-heading">Find biblical references in texts</p>
-                        </div>
-                      </a>
-                    </div>
-                  </div>
-                  <div class="close-btn-div">
-                    <a class="common-close-btn"><i class="close-btn-icon fa fa-times"></i>Close</a>
-                  </div>
-                </div>
-            </ul>
           </li>
         </ul>
-    <div class="mobile-header d-lg-none">
-      <div class="container p-x-10">
-        <div class="row no-gutters mobile-header-row">
-          <div class="col-xs-8">
-            <div class="logo-header">
-              <!--<router-link to="/">-->
-              <!--<img src="../assets/header-logo.png">-->
-              <!--</router-link>-->
-              <a class="logo-holder" href="/">
-                <img src="../assets/blue-logo.png" class="logo-img">
-                <span class="logo-text">Bible search</span>
-              </a>
-            </div>
-            <a class="dicta-link" href="#">DICTA</a>
-          </div>
-          <div class="col-xs-4">
-            <a class="mobile-link" @click="changeLanguage">עברית </a>
-          </div>
-        </div>
       </div>
     </div>
-  </header>
   </div>
 </template>
 <script>
-// import $ from 'jquery'
+import { tools } from './toolList'
 
 export default {
   name: 'Header',
@@ -116,41 +46,99 @@ export default {
   },
   data () {
     return {
-      menuOpen: false
+      menuOpen: true,
+      tools
     }
   },
   methods: {
     changeLanguage: function () {
+      this.hebrew = !this.hebrew
       this.$emit('lang', this.hebrew ? 'en' : 'he')
     },
-    openDropDown: function () {
+    toggleDropDown: function () {
       this.menuOpen = !this.menuOpen
     }
   }
 }
 </script>
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped src="./styles/common-header.css"></style>
 <style scoped>
-  .eng{
-    margin: 3px 15px 0 0;
+  .top-bar {
+    font-size: 13px;
+    line-height: 1.5;
+    font-family: Roboto, Arimo, sans-serif;
+    margin: 5px auto;
+    max-width: 1140px;
+    display: flex;
+    justify-content: space-between;
   }
-  .eng:before{
-    right: -9px;
+  .dicta {
+    font-size: 14px;
   }
-  .nav-mega {
-    float: right;
+  a {
+    color: black;
+    cursor: pointer;
   }
-  .link-icon{
-    margin: 0 0 0 4px;
+  a:hover {
+    color: black;
   }
-   .close-btn-icon {
-    margin: -1px 4px 0 0;
+  .popup {
+    position: relative;
   }
-   .close-btn-div {
-    right: 5px;
+  .popup-back {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
   }
-  .container {
-    max-width: 1400px;
+  .tool-bar {
+    box-shadow: 2px 2px 3px 0 rgba(0, 0, 0, 0.14);
+    border: solid 1px #d8d8d8;
+    box-sizing: border-box;
+    background-color: white;
+    z-index: 100000;
+    width: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+  .tool-list {
+    list-style: none;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: flex-start;
+    max-width: 1140px;
+    margin: auto;
+  }
+  .tool {
+    flex: 0 1 300px;
+    list-style: none;
+    margin: 20px 20px;
+    text-align: start;
+  }
+  .logo {
+    display: inline-block;
+    margin: 10px 7px;
+    height: 25px;
+    width: 25px;
+    background-color: white;
+    align-self: flex-start;
+  }
+  .description {
+    display: inline-block;
+  }
+  a:hover .title {
+    background-color: #d8d8d8;
+  }
+  .tool-link {
+    display: flex;
+    color: black;
+    text-decoration: none;
+  }
+  .title {
+    font-size: 18px;
+  }
+  .subtitle {
+    font-size: 13px;
   }
 </style>
---
+++ src/components/toolList.js
@@ -0,0 +1,41 @@
+export const tools = [
+  {
+    href: 'http://nakdan.dicta.org.il/',
+    logo: require('../assets/Logo_Nakdan_Black_Circle.png'),
+    engTitle: 'Nakdan',
+    engSubtitle: 'Add nikud to Modern / Rabbinic / Poetry text',
+    hebTitle: 'נקדן אוטומטי',
+    hebSubtitle: 'ניקוד טקסט מודרני / רבני / שירה'
+  },
+  {
+    href: 'http://classify.dicta.org.il',
+    logo: require('../assets/Logo_Classify_Black_Circle.png'),
+    engTitle: 'Bible Classification',
+    engSubtitle: 'Categorization, based on key styles markers',
+    hebTitle: 'סיווג טקסטים',
+    hebSubtitle: 'השוואת טקסטים בהתבסס על מאפיינים סיגונוניים'
+  },
+  {
+    href: 'http://synoptic.dicta.org.il/',
+    logo: require('../assets/Logo_Synoptic_Black_Circle.png'),
+    engTitle: 'Synopsis Builder',
+    engSubtitle: 'Aligns versions of the same text',
+    hebTitle: 'השוואת גרסאות',
+    hebSubtitle: 'השוואה בין גרסאות של אותו טקסט'
+  },
+  {
+    href: 'http://search.dicta.org.il/',
+    logo: require('../assets/Logo_Search_Black_Circle.png'),
+    engTitle: 'Search the Bible',
+    engSubtitle: 'Easily search in the Bible',
+    hebTitle: 'חיפוש בתנ״ך',
+    hebSubtitle: 'מנוע חיפוש אינטואיטיבי לתנ״ך'
+  },
+  {
+    href: 'http://pasuk.dicta.org.il/',
+    logo: require('../assets/Logo_PasukFinder_Black_Circle.png'),
+    engTitle: 'Pasuk Finder',
+    engSubtitle: 'Find biblical references in texts',
+    hebTitle: 'איזכורים תנ״כיים',
+    hebSubtitle: 'איתור איזכורים תנ״כיים בכל טקסט נתון'
+  }]
