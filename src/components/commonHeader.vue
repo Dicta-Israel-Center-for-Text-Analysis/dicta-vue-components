48cdfb72217a50788e4febbf2521c02361f6bb8f
Rivka Sharfman <rivkasa@gmail.com>
2019-12-23 11:19:53 +0200

    add cookie domain

--- src/components/commonHeader.vue
+++ src/components/commonHeader.vue
@@ -94,7 +94,7 @@ export default {
         this.$emit('lang-changed', this.hebrew ? 'en' : 'he')
       } else {
         this.$settings.hebrew = !this.$settings.hebrew
-        this.$cookies.set('DICTA_USE_HEBREW', this.$settings.hebrew, '3y')
+        this.$cookies.set('DICTA_USE_HEBREW', this.$settings.hebrew, '3y', '/', 'netlify.com')
         if (state.options.useBodyClass) {
           if (this.$settings.hebrew) {
             document.body.classList.add('he')
