ecdd3dc75e48108df03367c84fee62f867d8d3cc
Rivka Sharfman <rivkasa@gmail.com>
2020-01-30 13:22:34 +0200

    tool list

--- src/components/toolList.js
+++ src/components/toolList.js
@@ -1,6 +1,7 @@
 export const tools = [
   {
-    href: 'https://nakdan.dicta.org.il/',
+    engHref: 'https://nakdan.dicta.org.il/?lang=en',
+    hebHref: 'https://nakdan.dicta.org.il/',    
     logo: require('../assets/Logo Nakdan_Blue_Circle.png'),
     engTitle: 'Nakdan',
     engSubtitle: 'Add nikud to modern / rabbinic / poetic text',
@@ -16,7 +17,8 @@ export const tools = [
     hebSubtitle: 'השוואת טקסטים בהתבסס על מאפיינים סיגנוניים'
   },
   {
-    href: 'https://synoptic.dicta.org.il/',
+    engHref: 'https://synoptic.dicta.org.il/?lang=en',
+    hebHref: 'https://synoptic.dicta.org.il/',
     logo: require('../assets/Logo Synoptic_Blue_Circle.png'),
     engTitle: 'Synopsis Builder',
     engSubtitle: 'Align alternate versions of a text',
@@ -32,7 +34,8 @@ export const tools = [
     hebSubtitle: 'נקדן עם ממשק מותאם לאנשי מקצוע'
   },
   {
-    href: 'https://citation.dicta.org.il/',
+    engHref: 'https://citation.dicta.org.il/?lang=en',
+    hebHref: 'https://citation.dicta.org.il/',
     logo: require('../assets/Logo Pasuk Finder_Blue_Circle.png'),
     engTitle: 'Citation Finder',
     engSubtitle: 'Find biblical and talmudic citations',
@@ -40,8 +43,8 @@ export const tools = [
     hebSubtitle: 'איתור אזכורים תנ״כיים ותלמודיים בכל טקסט'
   },
   {
-    engHref: 'https://search.dicta.org.il/',
-    hebHref: 'https://search.dicta.org.il/he',
+    engHref: 'https://search.dicta.org.il/?lang=en',
+    hebHref: 'https://search.dicta.org.il/',
     logo: require('../assets/Logo Search_Blue_Circle.png'),
     engTitle: 'Search the Bible',
     engSubtitle: 'Find what you\'re really looking for in the Bible',
@@ -49,8 +52,8 @@ export const tools = [
     hebSubtitle: 'מנוע חיפוש אינטואיטיבי לתנ״ך'
   },
   {
-    engHref: 'https://segment.dicta.org.il/',
-    hebHref: 'https://segment.dicta.org.il/he',
+    engHref: 'https://segment.dicta.org.il/?lang=en',
+    hebHref: 'https://segment.dicta.org.il/',
     logo: require('../assets/Logo Segmentation_Blue_Circle.png'),
     engTitle: 'Stylistic Segmentation',
     engSubtitle: 'Partition text using stylistic markers',
@@ -66,8 +69,8 @@ export const tools = [
     hebSubtitle: 'זיהוי ופיענוח ראשי תיבות בטקסטים רבניים'
   },
   {
-    engHref: 'https://talmudsearch.dicta.org.il/',
-    hebHref: 'https://talmudsearch.dicta.org.il/he',
+    engHref: 'https://talmudsearch.dicta.org.il/?lang=en',
+    hebHref: 'https://talmudsearch.dicta.org.il/',
     logo: require('../assets/Logo Search_Talmud_Blue_Circle.png'),
     engTitle: 'Search the Talmud',
     engSubtitle: 'Find what you\'re really looking for in the Talmud',
