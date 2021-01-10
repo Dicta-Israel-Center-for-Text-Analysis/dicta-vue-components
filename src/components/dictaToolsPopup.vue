6574659f7b7068471f122a7b274332421b35bcf1
Rivka Sharfman <rivkasa@gmail.com>
2021-01-10 11:16:36 +0200

    check for cookies in local tool before setting

--- src/components/dictaToolsPopup.vue
+++ src/components/dictaToolsPopup.vue
@@ -157,7 +157,9 @@ export default {
         this.$emit('lang-changed', this.hebrew ? 'en' : 'he')
       } else {
         this.$settings.hebrew = !this.$settings.hebrew
-        this.$cookies.set('DICTA_USE_HEBREW', this.$settings.hebrew, -1, '/', 'dicta.org.il')
+        if (this.$cookies) {
+          this.$cookies.set('DICTA_USE_HEBREW', this.$settings.hebrew, -1, '/', 'dicta.org.il')
+        }  
         if (state.options.useBodyClass) {
           if (this.$settings.hebrew) {
             document.body.classList.add('he')
