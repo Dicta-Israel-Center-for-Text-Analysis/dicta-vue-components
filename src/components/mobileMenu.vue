ad4527dd8cf26f3ab9190e671303e5e2f9165479
Rivka Sharfman <rivkasa@gmail.com>
2021-03-04 12:13:38 +0200

    dir for menu in hebrew

--- src/components/mobileMenu.vue
+++ src/components/mobileMenu.vue
@@ -1,12 +1,5 @@
 <template>
-    <div class="mobile-menu">
-      <a
-        id="mobile-toolbar-button"
-        @click="dictaToolsMode.showDictaToolsPopup = true"
-        class="text-white"
-        >
-        <i class="fas fa-bars"></i>
-      </a>
+    <div class="mobile-menu" :dir="computedHebrew ? 'ltr' : 'rtl'">
       <a
         id="mobile-about-button"
         v-if="howDialog"
@@ -14,6 +7,13 @@
         @click="openHowItWorks"
         >
         <i class="fa fa-info-circle mx-3"></i>
+      </a>
+      <a
+        id="mobile-toolbar-button"
+        @click="dictaToolsMode.showDictaToolsPopup = true"
+        class="text-white"
+        >
+        <i class="fas fa-bars"></i>
       </a>
       <dicta-tools-popup :dicta-tools-mode="dictaToolsMode" :englishSupported="englishSupported"/>
     </div>
